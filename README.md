62c9cfc4e57f0c8e744b170dcbceedf86f14123e
王帅 <wayne@wangshuaide-MacBook-Pro.local>
2020-03-26 00:31:40 +0800

    JSON done, working on js

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ External packages:
 
 - Fuse - lightweight fuzzy-search by [Kiro Risk](https://kiro.me/), v5.0.7-beta
   - https://github.com/krisk/fuse
-
+- jQuery - of course
+  - Microsoft CDN: https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js
+  - Used on in main.js to load JSON data
--- main.js
+++ main.js
@@ -1,10 +1,52 @@
+
+let courseData
+
+$.getJSON('courses_data.json', function(data){
+	courseData = data;
+})
+
+
 function courseSearch(){
 	let keyword = document.getElementById("course_keyword").value;
-	document.getElementById("course_search_result").innerHTML = "keyword: " + keyword + "<br>Result:";
 	
+	let options = {
+		shouldSort: true,
+		threshold: 0.6,
+		distance: 100,
+		location: 0,
+		keys: [
+			"Course_title",
+			"Course_code",
+			"CRN",
+			"Room",
+			"Instructor"
+		]
+	};
+
+	let fuse = new Fuse(courseData, options);
+	fuse.search(keyword);
+
+	document.getElementById("course_search_result").innerHTML = "keyword: " + keyword + "<br>Result:" + fuse;
+
+	console.log(fuse);
+	/*
+	let options = {
+		shouldSort: true,
+		threshold: 0.6,
+		location: 0,
+		distance: 100,
+		minMatchCharLength: 1,
+		keys: [
+		"Course_title"
+		]
+	};
+	let fuse = new Fuse(courseData, options); // "list" is the item array
+	let result = fuse.search("comp");
+	*/
 
 
 }
+
 
 function checkRoomAval(){
 	let roomCode = document.getElementById('room_code').value;
